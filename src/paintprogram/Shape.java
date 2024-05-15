02fe35fd51b590af09878df3231955651715352f
michael.roy-diclemen <michael.roy-diclemen@TEACH2D-C3NYBS3.EDU.YRDSB.NET>
2024-05-15 08:47:20 -0400

    v1.1

---
+++ src/paintprogram/Shape.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paintprogram;
+
+/**
+ *
+ * @author michael.roy-diclemen
+ */
+public interface Shape {
+ /**
+  * Will Scale the given shape by the integer amount given
+  * @param amt 
+  */   
+  public void scale(int amt);
+}
